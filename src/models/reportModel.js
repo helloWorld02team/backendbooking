197eea83153f89faa5a9b05b592a48a672bd2a6e
Ex0TiiC24 <taiyophamadadar6@gmail.com>
2025-02-04 01:09:07 +0700

    nothing

--- src/models/reportModel.js
+++ src/models/reportModel.js
@@ -1,7 +1,7 @@
 import db from '../configs/database.js'
 
 export const createReportInfo = async (details) =>{ await db.promise().query(
-    `INSERT INTO Report (ReportMessage,Room_idRoom,ReporterName,ReportTime) VALUES (?,?,?,?)`,[details.report,details.room,details.reporterName,details.time]
+    `INSERT INTO Report (ReportMessage,Room_idRoom,ReporterName,ReportDate) VALUES (?,?,?,?)`,[details.report,details.room,details.reporterName,details.time]
 );
 return details
 }
