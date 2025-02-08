92a5b502f2721f2a64ec3fa10eebfa049b04c707
Ex0TiiC24 <taiyophamadadar6@gmail.com>
2025-02-08 17:07:00 +0700

    fix

--- src/utils/cookieAuthen.js
+++ src/utils/cookieAuthen.js
@@ -17,7 +17,6 @@ export const verifyToken = (req) => {
 export const setTokenCookie = (res, token) => {
     res.cookie("token", token, {
         httpOnly: false,
-        sameSite: "Lax",
         maxAge: 86400000
     });
 };
