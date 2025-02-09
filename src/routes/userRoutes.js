ff8a7d51a1ceeb8b5c95739a10169946bb3b3d02
Ex0TiiC24 <taiyophamadadar6@gmail.com>
2025-02-09 10:40:48 +0700

    fix

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,8 +4,6 @@ import express from 'express'
 
 const userRoutes = express.Router();
 
-userRoutes.get("/getalluser",userControllers.getUser);
-
 userRoutes.post("/login",validateLogin,userControllers.loginUser);
 
 userRoutes.get('/getuserdata', userControllers.getUserData);
